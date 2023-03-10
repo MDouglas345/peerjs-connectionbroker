801d1a9c1723258143c855064aace55a1d29454d
michaeldouglas <michael_gcm@hotmail.com>
2023-03-10 14:24:27 -0700

    peerjs testing

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const { PeerServer } = require("peer");
 
-const peerServer = PeerServer({ port: 9000, path: "/myapp" });
+const peerServer = PeerServer({ port: 9000 });
 
 const clients = {};
 const hosts = {};
 
 
 peerServer.on('connection', (client) =>{
-    console.log("wow");
+    clients[client.id] = client;
+    console.log(clients);
+});
+
+
+peerServer.post('/host', (req, res) => {
+    
 });
 
 //console.log(peerServer);
