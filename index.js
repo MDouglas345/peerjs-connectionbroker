425db59e2d733184c8ea283ca00eb9935a654368
michaeldouglas <michael_gcm@hotmail.com>
2023-03-15 10:44:58 -0600

    confirming pt2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { PeerServer } = require("peer");
 
-const peerServer = PeerServer({ port: 9000 });
+const peerServer = PeerServer({ port: 9000, path : "/broker" });
 
 const clients = {};
 const hosts = {};
